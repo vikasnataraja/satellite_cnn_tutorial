897eae61156dc8c3bd10002842ff9c5f163fd961
Vikas Nataraja <hnvikas14@gmail.com>
2024-11-17 23:13:35 -0700

    update packages to have keras before tf

--- install_packages.sh
+++ install_packages.sh
@@ -4,6 +4,6 @@ conda activate cnn
 conda install -c conda-forge numpy matplotlib jupyterlab -y
 conda install -c anaconda pillow -y
 conda install -c conda-forge xarray dask netCDF4 bottleneck -y
-conda install tensorflow==2.12.0 -y
 conda install -c conda-forge keras==2.12.0 -y
+conda install tensorflow==2.12.0 -y
 echo "finished installing conda environment cnn"
